fff37b4150010738ae5f127452cf55c57f995cd5
Eadweard <804875728@qq.com>
2023-07-19 17:58:09 +0800

    fix

--- booking-frontend/vite.config.ts
+++ booking-frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssPresetEnv from "postcss-preset-env"
+import cssnext from "postcss-cssnext"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [postcssPresetEnv()]
+      plugins: [postcssPresetEnv(), cssnext()]
     }
   }
 })
