216406d940f0270dbecc42ef7821f576c1d4060e
Eadweard <804875728@qq.com>
2023-07-19 22:57:15 +0800

    tailwind

--- booking-frontend/src/config/router.ts
+++ booking-frontend/src/config/router.ts
@@ -1,1 +1,20 @@
 
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  createBrowserRouter
+} from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "team",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],
+  },
+]);
