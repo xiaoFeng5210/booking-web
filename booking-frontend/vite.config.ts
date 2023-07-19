58e47ce3ef5546d65d2bf68a22d295c55c140499
Eadweard <804875728@qq.com>
2023-07-19 17:37:18 +0800

    types node

--- booking-frontend/vite.config.ts
+++ booking-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const postcssPresetEnv = require("postcss-preset-env") 
 
 // https://vitejs.dev/config/
 export default defineConfig({
