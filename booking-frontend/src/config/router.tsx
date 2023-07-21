fc36886dca23c657e250ea97375a59833c0d76fe
Eadweard <804875728@qq.com>
2023-07-21 14:12:06 +0800

    fix postcss

---
+++ booking-frontend/src/config/router.tsx
@@ -0,0 +1,12 @@
+import React, { FC, lazy } from "react";
+import { useRoutes } from "react-router-dom";
+
+const Welcome = lazy(() => import("~/pages/Welcome"));
+
+const RouterConfig: FC = () => {
+  return useRoutes([
+    { path: "/", element: <Welcome />},
+  ]);
+};
+
+export default RouterConfig;
